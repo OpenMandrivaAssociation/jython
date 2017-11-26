6cc75af0906aa64329ae02e7b3cf570a93452354
mandian-lx <mandian-lx@users.noreply.github.com>
2017-11-26 13:55:59 +0100

    2.7.1

--- fetch-jython.sh
+++ fetch-jython.sh
@@ -7,10 +7,12 @@ TAG=$1
 
 rm -rf jython-$TAG
 rm -rf jython-$TAG.tar.xz
-hg clone -r "$TAG" http://hg.python.org/jython jython-$TAG
+hg clone -r "$TAG" https://hg.python.org/jython jython-$TAG
 
 find jython-$TAG -type f -a -name *.jar -delete
 find jython-$TAG -type f -a -name *.class -delete
+find jython-$TAG -type f -a -name *.exe -delete
+find jython-$TAG -type f -a -name *.dll -delete
 rm -rf jython-$TAG/.hg
 
 tar Jcf jython-$TAG.tar.xz jython-$TAG
